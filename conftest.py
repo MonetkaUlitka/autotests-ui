60df0e79c1ee108b911673db90e8cbf9c433b3af
MonetkaUlitka <ra7270680@gmail.com>
2026-04-24 19:28:14 +0500

    init

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import os
+import pytest
+from playwright.sync_api import sync_playwright
+
+
+@pytest.fixture
+def browser():
+    with sync_playwright() as p:
+        # Запускаем браузер (headless=False показывает окно)
+        browser = p.chromium.launch(headless=False, slow_mo=800)
+        yield browser
+        browser.close()
+
+@pytest.fixture
+def page(browser):
+    # Создаем новую страницу для каждого теста
+    page = browser.new_page()
+    yield page
+    page.close()
