36328f042f7841d6114ade7ae81027779db2b105
MonetkaUlitka <ra7270680@gmail.com>
2026-04-29 17:52:07 +0500

    add courses test

---
+++ tests/playwright_courses.py
@@ -0,0 +1,30 @@
+from playwright.sync_api import Page, expect 
+
+def test_registration(page:Page, context):
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    email_input= page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user_email@gmail.com')
+
+    username_input= page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('someUser')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    context.storage_state(path='browser-state.json')
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+    courses_header = page.get_by_test_id('courses-list-toolbar-title-text')
+    courses_text = page.get_by_test_id('courses-list-empty-view-title-text')
+
+    context.storage_state(path='browser-state.json')
+
+    expect(courses_header).to_be_visible()
+    expect(courses_header).to_have_text('Courses')
+    expect(courses_text).to_be_visible()
+    expect(courses_text).to_have_text('There is no results')
