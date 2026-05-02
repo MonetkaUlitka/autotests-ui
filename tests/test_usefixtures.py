9dc77c22d0888ab62f49014ae33f498d6bdbeb98
MonetkaUlitka <ra7270680@gmail.com>
2026-05-02 17:56:31 +0500

    add mark usefixture example

--- tests/test_usefixtures.py
+++ tests/test_usefixtures.py
@@ -12,3 +12,11 @@ def fill_books_database() -> None:
 @pytest.mark.usefixtures('fill_books_database')
 def test_read_all_books_in_library():
     print('Reading all books')
+
+@pytest.mark.usefixtures('clear_books_databse', 'fill_books_database')
+class TestLibrary:
+    def test_read_book_from_library(self):
+        ...
+
+    def test_delete_book_from_library(self):
+        ...
