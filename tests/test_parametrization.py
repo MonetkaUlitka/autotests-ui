9a002bd2757695f4a6047424204d25febc4bd2db
MonetkaUlitka <ra7270680@gmail.com>
2026-05-04 16:58:11 +0500

    Динамические идентификаторы в параметризации example

--- tests/test_parametrization.py
+++ tests/test_parametrization.py
@@ -54,3 +54,20 @@ class TestOperations:
 )
 def test_identifiers(phone_number: str):
     pass
+
+
+
+# Словарь пользователей: номер телефона — ключ, описание — значение
+users = {
+    "+70000000011": "User with money on bank account",
+    "+70000000022": "User without money on bank account",
+    "+70000000033": "User with operations on bank account"
+}
+
+@pytest.mark.parametrize(
+    "phone_number", 
+    users.keys(),  # Передаем список номеров телефонов
+    ids=lambda phone_number: f"{phone_number}: {users[phone_number]}"  # Генерируем идентификаторы динамически
+)
+def test_identifiers(phone_number: str):
+    pass
