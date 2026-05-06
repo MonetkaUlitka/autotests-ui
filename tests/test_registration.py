f7ee183b0ba68d414b390b9a2c1e4cd80221c8b9
MonetkaUlitka <ra7270680@gmail.com>
2026-05-06 15:16:06 +0500

    registration test with new pages and fixtures

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,41 +1,31 @@
-from playwright.sync_api import Page, expect
 import pytest
+from playwright.sync_api import Page
+from pages.registration_page import RegistationPage
+from pages.dashboard_page import DashboardPage
+
 
 @pytest.mark.regression
-def test_succses_registration(page:Page):
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+def test_successful_registration(registration_page:RegistationPage, dashboard_page: DashboardPage):
+    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
+    registration_page.fill_registration_form(email='ra123@gmail.com', username="SomeUser", password='helloWorld')
+    registration_page.click_registration_button()
+    dashboard_page.check_dashboard_header()
 
-    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill('username')
+# @pytest.mark.smoke
+# def test_disabled_registration_button(page:Page):
+#     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill('password')
+#     registration_button = page.get_by_test_id('registration-page-registration-button')
+#     expect(registration_button).to_be_disabled()
 
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
+#     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+#     email_input.fill('user.name@gmail.com')
 
-    dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
+#     username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+#     username_input.fill('username')
 
-    expect(dashboard_header).to_be_visible()
-    expect(dashboard_header).to_have_text('Dashboard')
+#     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+#     password_input.fill('password')
 
-@pytest.mark.smoke
-def test_disabled_registration_button(page:Page):
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    expect(registration_button).to_be_disabled()
-
-    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
-
-    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill('username')
-
-    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill('password')
-
-    expect(registration_button).not_to_be_disabled()
+#     expect(registration_button).not_to_be_disabled()
