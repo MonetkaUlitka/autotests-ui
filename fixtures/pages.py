1309ba1183536314cd8f46b609a0d007d464d943
MonetkaUlitka <ra7270680@gmail.com>
2026-05-06 14:32:29 +0500

    create new fixture - registation page

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -2,8 +2,14 @@ import pytest
 from playwright.sync_api import Page
 
 from pages.login_page import LoginPage
+from pages.registration_page import RegistationPage
 
 
 @pytest.fixture
 def login_page(chromium_page: Page) -> LoginPage:
     return LoginPage(page=chromium_page)
+
+
+@pytest.fixture
+def registration_page(chromium_page:Page) -> RegistationPage:
+    return RegistationPage(page=chromium_page)
