fcfac8c7441bf6f024e15d733f5cc18e046fce91
MonetkaUlitka <ra7270680@gmail.com>
2026-05-07 13:54:06 +0500

    rename the methods in dashbord locators

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -8,14 +8,18 @@ class DashboardPage(BasePage):
 
         self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
         self.dashboard_button = page.get_by_test_id('dashboard-drawer-list-item-button')
-        self.student_widget_title = page.get_by_test_id('students-widget-title-text')
-        self.student_bar = page.get_by_test_id('students-bar-chart')
-        self.activities_widget_title = page.get_by_test_id('activities-widget-title-text')
-        self.activities_line = page.get_by_test_id('activities-line-chart')
-        self.courses_widget_title = page.get_by_test_id('courses-widget-title-text')
-        self.courses_pie = page.get_by_test_id('courses-pie-chart')
-        self.scores_widget_title = page.get_by_test_id('scores-widget-title-text')
-        self.scores_scatter = page.get_by_test_id('scores-scatter-chart')
+
+        self.student_title = page.get_by_test_id('students-widget-title-text')
+        self.student_chart = page.get_by_test_id('students-bar-chart')
+
+        self.activities_title = page.get_by_test_id('activities-widget-title-text')
+        self.activities_chart = page.get_by_test_id('activities-line-chart')
+
+        self.courses_title = page.get_by_test_id('courses-widget-title-text')
+        self.courses_chart = page.get_by_test_id('courses-pie-chart')
+
+        self.scores_title = page.get_by_test_id('scores-widget-title-text')
+        self.scores_chart = page.get_by_test_id('scores-scatter-chart')
 
     def check_visible_dashboard_title(self):
         expect(self.dashboard_title).to_be_visible()
