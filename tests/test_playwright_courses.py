e975d0f56dbba3a3861d1b1d182b5bd911e2c2be
MonetkaUlitka <ra7270680@gmail.com>
2026-05-16 15:16:32 +0500

    Update test_playwright_courses.py

--- tests/test_playwright_courses.py
+++ tests/test_playwright_courses.py
@@ -8,7 +8,7 @@ import pytest
 def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
-    courses_list_page.navbar.check_visible()
+    courses_list_page.navbar.check_visible("username")
     courses_list_page.sidebar.check_visible()
 
     courses_list_page.check_visible_courses_title()
