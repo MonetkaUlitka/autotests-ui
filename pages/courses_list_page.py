330ab9a2c6770fef4971ec17e01a3d844bad85ce
MonetkaUlitka <ra7270680@gmail.com>
2026-05-18 16:12:17 +0500

    Update courses_list_page.py

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -3,6 +3,7 @@ from pages.base_page import BasePage
 from components.navigation.sidebar_component import SidebarComponent
 from components.navigation.navbar_component import NavbarComponent
 from components.views.empty_view_compoments import EmptyViewComponent
+from components.courses.course_view_component import CourseViewComponent
 
 
 class CoursesListPage(BasePage):
@@ -11,36 +12,11 @@ class CoursesListPage(BasePage):
         self.sidebar = SidebarComponent(page)
         self.navbar = NavbarComponent(page)
         self.empty_view = EmptyViewComponent(page, identifier= 'courses-list')
+        self.course_view = CourseViewComponent(page)
 
         #Заголовок и кнопка создания курса
         self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text') 
         self.courses_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
-
-        # Карточка курса 
-        self.course_image = page.get_by_test_id('course-preview-image')
-        self.course_title = page.get_by_test_id('course-widget-title-text')
-
-        self.course_max_text = page.get_by_test_id('course-max-score-info-row-view-text')
-        self.course_max_icon = page.get_by_test_id('KeyboardDoubleArrowUpIcon')
-        
-        self.course_min_text = page.get_by_test_id('course-min-score-info-row-view-text')
-        self.course_min_icon = page.get_by_test_id('KeyboardDoubleArrowDownIcon')
-
-        self.course_estimated_time_text = page.get_by_test_id('course-estimated-time-info-row-view-text')
-        self.course_estimated_icon = page.get_by_test_id('AccessTimeOutlinedIcon')
-
-
-
-        # Меню курса
-        self.course_menu_button = page.get_by_test_id('course-view-menu-button')
-
-        self.course_edit_button = page.get_by_test_id('course-view-edit-menu-item-text')
-        self.course_edit_icon = page.get_by_test_id('EditOutlinedIcon')
-
-        self.course_delete_button = page.get_by_test_id('course-view-delete-menu-item-text')
-        self.course_delete_icon = page.get_by_test_id('DeleteOutlineOutlinedIcon')
-
-
 
 
     def check_visible_courses_title(self):
@@ -59,31 +35,6 @@ class CoursesListPage(BasePage):
 
     def click_create_course_button(self):
         self.courses_button.click()
-
-    def check_visible_course_card(self, index:int, title:str, max_score:str, min_score:str, estimated_time:str):
-        expect(self.course_image.nth(index)).to_be_visible()
-
-        expect(self.course_title.nth(index)).to_be_visible()
-        expect(self.course_title.nth(index)).to_have_text(title)
-
-        expect(self.course_max_text.nth(index)).to_be_visible()
-        expect(self.course_max_text.nth(index)).to_have_text(f"Max score: {max_score}")
-
-        expect(self.course_min_text.nth(index)).to_be_visible()
-        expect(self.course_min_text.nth(index)).to_have_text(f"Min score: {min_score}")
-
-        expect(self.course_estimated_time_text.nth(index)).to_be_visible()
-        expect(self.course_estimated_time_text.nth(index)).to_have_text(f"Estimated time: {estimated_time}")
-
-    def click_edit_course_button(self, index:int):
-        self.course_menu_button.nth(index).click()
-        expect(self.course_edit_button.nth(index)).to_be_visible()
-        self.course_edit_button.click()
-
-    def click_delete_course_button(self, index:int):
-        self.course_menu_button.nth(index).click()
-        expect(self.course_delete_button.nth(index)).to_be_visible()
-        self.course_delete_button.click()
 
 
     
