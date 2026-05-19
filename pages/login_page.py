0f58a3e58ec2923118315aeb9d6c862632aead6b
MonetkaUlitka <ra7270680@gmail.com>
2026-05-19 16:24:49 +0500

    fix login_form

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,7 +6,7 @@ from components.authentication.login_form_component import LoginFormComponent
 class LoginPage(BasePage):
     def __init__(self, page:Page):
         super().__init__(page)
-        login_form = LoginFormComponent(page)
+        self.login_form = LoginFormComponent(page)
 
         self.login_button = page.get_by_test_id('login-page-login-button')
         self.registration_link = page.get_by_test_id('login-page-registration-link')
