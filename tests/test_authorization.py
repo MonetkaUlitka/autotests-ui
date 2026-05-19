0ad32ca3dec8167e07d9b1d22884f540091d113a
MonetkaUlitka <ra7270680@gmail.com>
2026-05-19 16:24:53 +0500

    Update test_authorization.py

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -2,7 +2,7 @@ import pytest
 from playwright.sync_api import Page
 from pages.login_page import LoginPage
 
-
+@pytest.mark.regression
 @pytest.mark.parametrize("email, password", [
     ("user.name@gmail.com", "password"),
     ("user.name@gmail.com", "  "), 
@@ -12,6 +12,6 @@ from pages.login_page import LoginPage
 def test_wrong_email_or_password_authorization(login_page:LoginPage, email:str, password:str):
     login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
 
-    login_page.fill_login_form(email=email, password=password)
+    login_page.login_form.fill(email, password)
     login_page.click_login_button()
-    login_page.check_visible_wrong_email_or_password()
+    login_page.check_visible_wrong_email_or_password() 
