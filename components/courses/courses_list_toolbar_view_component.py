5b8e2302699723f3a50b2498f12ba9c6785b8215
MonetkaUlitka <ra7270680@gmail.com>
2026-05-19 14:04:39 +0500

    Update courses_list_toolbar_view_component.py

--- components/courses/courses_list_toolbar_view_component.py
+++ components/courses/courses_list_toolbar_view_component.py
@@ -11,11 +11,11 @@ class CoursesListToolbarViewComponent(BaseComponent):
         self.create_courses_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
 
     def check_visible(self):
-        expect(self.courses_title).to_be_visible()
-        expect(self.courses_title).to_have_text('Courses')
+        expect(self.title).to_be_visible()
+        expect(self.title).to_have_text('Courses')
 
         expect(self.create_courses_button).to_be_visible()
 
     def click_create_course_button(self):
-        self.courses_button.click()
+        self.create_courses_button.click()
         self.check_current_url(re.compile(".*/#/courses/create"))
