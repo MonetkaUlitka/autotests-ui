e0ec622e8ba2ed8e1df02962ff0167312551d282
MonetkaUlitka <ra7270680@gmail.com>
2026-05-20 16:53:57 +0500

    Update courses_view_menu_component.py

--- components/courses/courses_view_menu_component.py
+++ components/courses/courses_view_menu_component.py
@@ -1,25 +1,29 @@
 from playwright.sync_api import Page, expect 
 from components.base_component import BaseComponent
+from elements.button import Button
+from elements.icon import Icon
 
 class CourseViewMenuComponent(BaseComponent):
     def __init__(self, page:Page):
         super().__init__(page)
 
          # Меню курса
-        self.menu_button = page.get_by_test_id('course-view-menu-button')
+        self.menu_button = Button(page, locator='course-view-menu-button', name='menu')
 
-        self.edit_menu_item = page.get_by_test_id('course-view-edit-menu-item-text')
-        self.course_edit_icon = page.get_by_test_id('EditOutlinedIcon')
+        self.edit_menu_item = Button(page, locator='course-view-edit-menu-item-text', name='Edit')
+        self.course_edit_icon = Icon(page, locator='EditOutlinedIcon', name='Edit Icon')
 
-        self.delete_menu_item = page.get_by_test_id('course-view-delete-menu-item-text')
-        self.course_delete_icon = page.get_by_test_id('DeleteOutlineOutlinedIcon')
+        self.delete_menu_item = Button(page, locator='course-view-delete-menu-item-text', name='Delete')
+        self.course_delete_icon = Icon(page, locator='DeleteOutlineOutlinedIcon', name='Delete Icon')
 
     def click_edit(self, index:int):
-        self.menu_button.nth(index).click()
-        expect(self.edit_menu_item.nth(index)).to_be_visible()
-        self.edit_menu_item.click()
+        self.menu_button.click(nth=index)
+        
+        self.edit_menu_item.check_visible(nth=index)
+        self.edit_menu_item.click(nth=index)
 
     def click_delete(self, index:int):
-        self.menu_button.nth(index).click()
-        expect(self.delete_menu_item.nth(index)).to_be_visible()
-        self.delete_menu_item.click()
+        self.menu_button.click(nth=index)
+        
+        self.delete_menu_item.check_visible(nth=index)
+        self.delete_menu_item.click(nth=index) 
