bbd5a110ff276dea25b2d8573b68e763b44ba12f
MonetkaUlitka <ra7270680@gmail.com>
2026-05-20 18:11:15 +0500

    Update registration_page.py

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,14 +1,16 @@
 from playwright.sync_api import Page, expect 
 from pages.base_page import BasePage
 from components.authentication.registration_form_component import RegistrationFormComponent
+from elements.link import Link
+from elements.button import Button 
 
 class RegistationPage(BasePage):
     def __init__(self, page:Page):
         super().__init__(page)
         self.registration_form = RegistrationFormComponent(page)
 
-        self.login_link = page.get_by_test_id('registration-page-login-link')
-        self.registration_button = page.get_by_test_id('registration-page-registration-button')
+        self.login_link = Link(page, 'registration-page-login-link', 'Login Link')
+        self.registration_button = Button(page, 'registration-page-registration-button', 'Registration Button')
 
        
     def click_registration_button(self):
